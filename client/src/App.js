c1fb465d0bbb2e1510c990acea3dc7276335bbd4
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-05-04 02:17:14 +0300

    updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserForm from "./components/forms/UserForm";
+import ManufacturerForm from "./components/forms/ManufacturerForm";
+import "./App.css"; // Import global styles for the app
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="app-header">
+          <h1>Welcome to the Offer Optimization App</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<UserForm />} />
+            <Route path="/manufacturer" element={<ManufacturerForm />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
